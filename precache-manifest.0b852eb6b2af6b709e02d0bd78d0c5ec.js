ccdd7e248655fb25bf2c1172bc530431b50f9625
Sentenzos <49534576+Sentenzos@users.noreply.github.com>
2020-02-03 15:17:43 +0300

    Updates

---
+++ precache-manifest.0b852eb6b2af6b709e02d0bd78d0c5ec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5039a5afc643a01c85284cc28b1f6e8",
+    "url": "/qq/index.html"
+  },
+  {
+    "revision": "a7271c51a800170ec258",
+    "url": "/qq/static/css/2.59f4f2a6.chunk.css"
+  },
+  {
+    "revision": "b07e3d4538637cc29d00",
+    "url": "/qq/static/css/main.ab06486d.chunk.css"
+  },
+  {
+    "revision": "a7271c51a800170ec258",
+    "url": "/qq/static/js/2.54442579.chunk.js"
+  },
+  {
+    "revision": "b07e3d4538637cc29d00",
+    "url": "/qq/static/js/main.b3dac5bd.chunk.js"
+  },
+  {
+    "revision": "86463323c725c1f6759a",
+    "url": "/qq/static/js/runtime-main.ae6e2eb9.js"
+  },
+  {
+    "revision": "261c625b74ecba79436e4ba6459939b2",
+    "url": "/qq/static/media/demoAva1.261c625b.jfif"
+  },
+  {
+    "revision": "165e1dc56a12a8ca5ee2f208e497313e",
+    "url": "/qq/static/media/demoAva2.165e1dc5.jfif"
+  },
+  {
+    "revision": "eb4dab2558a9f1107d2d9078493dabec",
+    "url": "/qq/static/media/loading.eb4dab25.svg"
+  },
+  {
+    "revision": "816dc0f76ca5e688a0587481c60575d8",
+    "url": "/qq/static/media/news.816dc0f7.png"
+  },
+  {
+    "revision": "e1ab532417d7600afe57503976dc881f",
+    "url": "/qq/static/media/profile.e1ab5324.png"
+  },
+  {
+    "revision": "2a5980a153b36250081fe623a8207dea",
+    "url": "/qq/static/media/users.2a5980a1.png"
+  }
+]);
